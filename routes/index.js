9b63ddb6795abdc0b01fb294350e10c833ae555a
artslimedev <102567983+artslimedev@users.noreply.github.com>
2022-08-11 01:00:39 -0400

    pushing brad google auth hw

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
+const Story = require('../models/Story')
+
+// @desc Login/Landing page
+//@route GET /
+router.get('/', ensureGuest, (req,res) => {
+    res.render('Login', 
+    {
+        layout: 'login',
+    })
+})
+
+// @desc Login/Landing page
+//@route GET /dashboard
+router.get('/dashboard', ensureAuth, async (req,res) => {
+    try{
+        const stories = await Story.find({ user: req.user.id}).lean()       
+        res.render('Dashboard',{
+        name: req.user.firstName,
+        stories
+    })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+})
+
+module.exports = router
